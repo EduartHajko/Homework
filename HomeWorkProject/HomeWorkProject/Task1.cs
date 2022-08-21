50c171f008357f713b9c0127882a8ae3120c93ee
Eduart <eduarthajko1992@gmail.com>
2022-08-21 12:30:56 +0200

    added try catch

--- HomeWorkProject/HomeWorkProject/Task1.cs
+++ HomeWorkProject/HomeWorkProject/Task1.cs
@@ -10,11 +10,20 @@ namespace HomeWorkProject
     {
         public static void Solution()
         {
-            Console.WriteLine("Type in circle diameter:");
-            string input = Console.ReadLine();
-            double diameter = double.Parse(input);
-            double perimeter = 2 * Math.PI * diameter;
-            Console.WriteLine($"Circle perimeter is {perimeter}");
+            try
+            {
+                Console.WriteLine("Type in circle diameter:");
+                string input = Console.ReadLine();
+                double diameter = double.Parse(input);
+                double perimeter = 2 * Math.PI * diameter;
+                Console.WriteLine($"Circle perimeter is {perimeter}");
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+           
         }
     }
 }
