64ca0874a6bbeaa3d440a964ff5e318b9c39cf8b
Eduart <eduarthajko1992@gmail.com>
2022-08-27 12:21:24 +0200

    ushtrimi 2

--- HomeWorkProject/HomeWorkProject/Program.cs
+++ HomeWorkProject/HomeWorkProject/Program.cs
@@ -6,7 +6,8 @@ public class SDA
     public static void Main()
     {
         //Ky kodi therret zgjidhjen nga klasa solution task 1
-        Task1.Solution();
+        //Task1.Solution();
+        e_h_Task3.Solution2();
         //Homework1.Solution();
     }
 }
--- HomeWorkProject/HomeWorkProject/e_h_Task3.cs
+++ HomeWorkProject/HomeWorkProject/e_h_Task3.cs
@@ -10,6 +10,7 @@ namespace HomeWorkProject
 
     public class e_h_Task3
     {
+        //ushtrimi 1
         public static void Solution()
         {
             StringBuilder sb = new StringBuilder();
@@ -30,5 +31,33 @@ namespace HomeWorkProject
             }
 
         }
+
+
+        //ushtrimi 2 
+        public static void Solution2()
+        {
+           
+                int[] arr = new int[10];
+                int i;
+                Console.Write("\n\nRead and Print elements of an array:\n");
+                Console.Write("-----------------------------------------\n");
+
+                Console.Write("Input 10 elements in the array :\n");
+                for (i = 0; i < 10; i++)
+                {
+                    Console.Write("element - {0} : ", i);
+                    arr[i] = Convert.ToInt32(Console.ReadLine());
+                }
+
+                Console.Write("\nElements in array are: ");
+                for (i = 0; i < 10; i++)
+                {
+                    Console.Write("{0}  ", arr[i]);
+                }
+                Console.Write("\n");
+
+        }
+
+
     }
 }
