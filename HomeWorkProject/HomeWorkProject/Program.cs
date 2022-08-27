feea65827a6f2f5781baa3ee2cd3061e55ecba1f
Armir <armir.keta@gmail.com>
2022-08-27 11:39:11 +0200

    added Program.cs file

--- HomeWorkProject/HomeWorkProject/Program.cs
+++ HomeWorkProject/HomeWorkProject/Program.cs
@@ -6,7 +6,8 @@ public class SDA
     public static void Main()
     {
         //Ky kodi therret zgjidhjen nga klasa solution task 1
-        Task1.Solution();
+        //Task1.Solution();
         //Homework1.Solution();
+        A_K_Task3.Solution2();
     }
 }
