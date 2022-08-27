dbf1331788331deadbbda1e338a995f9b0518a0d
Eduart <eduarthajko1992@gmail.com>
2022-08-27 10:26:48 +0200

    tasku 3

---
+++ HomeWorkProject/HomeWorkProject/e_h_Task3.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkProject
+{
+  
+
+    public class e_h_Task3
+    {
+        public static void Solution()
+        {
+            StringBuilder sb = new StringBuilder();
+
+
+            Console.WriteLine("Type in text");
+            string input = Console.ReadLine();
+
+            if (input.ToLower() != "stop")
+            {
+
+
+                sb.Append(input);
+            }
+            else
+            {
+                Console.WriteLine("rezultati i string builderit" + sb.ToString());
+            }
+
+        }
+    }
+}
