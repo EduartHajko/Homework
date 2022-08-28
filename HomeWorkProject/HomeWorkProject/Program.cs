23757157935088c3537140b0fe477df41b4d8e24
AG <AG@DESKTOP-O9E3P30>
2022-08-28 10:50:54 +0200

    puna e 2 diteve

--- HomeWorkProject/HomeWorkProject/Program.cs
+++ HomeWorkProject/HomeWorkProject/Program.cs
@@ -6,7 +6,10 @@ public class SDA
     public static void Main()
     {
         //Ky kodi therret zgjidhjen nga klasa solution task 1
-        Task1.Solution();
+        //Task1.Solution();
         //Homework1.Solution();
+        //D_SH_Task3.SolutionTask3();
+        D_SH_Task3.Solution4();
+
     }
 }
