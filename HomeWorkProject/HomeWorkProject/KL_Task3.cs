6cedd9e6be4531deb4c1948c26bd1d66aebd1a17
HP <109424024+KridensL@users.noreply.github.com>
2022-08-28 10:51:27 +0200

    II Commit.

---
+++ HomeWorkProject/HomeWorkProject/KL_Task3.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HomeWorkProject;
+
+namespace HomeWorkProject
+{
+    public class Solution
+    {
+        //    //public static void Solution1()
+        //    //{
+        //    //    StringBuilder sb = new StringBuilder();
+        //    //    Console.WriteLine("Type a word:");
+
+        //    //    string input = Console.ReadLine();
+
+        //    //    if (input.ToLower() != "stop")
+        //    //    {
+        //    //        sb.Append(input);
+        //    //    }
+        //    //    else
+        //    //    {
+        //    //        Console.WriteLine("StringBuilder result" + sb.ToString());
+        //    //    }
+
+        //    //}
+
+        //    //public static void Exercice6()
+        //    //{
+
+        //    //    try
+        //    //    {
+        //    //        Console.WriteLine("Write a number :");
+        //    //        string input = Console.ReadLine();
+        //    //        string[] Number = input.Split(",");
+
+        //    //        int[] numbers = new int[Number.Length];
+        //    //        for (int i = 0; i < numbers.Length; i++)
+        //    //        {
+        //    //            numbers[i] = Convert.ToInt32(Number[i]);
+
+        //    //        }
+        //    //        foreach (int number in numbers)
+        //    //        {
+        //    //            Console.WriteLine(number);
+        //    //        }
+        //    //    }
+        //    //    catch (Exception  EX) 
+        //    //    {
+
+        //    //        throw EX;
+        //    //    }
+
+        //    //}
+
+        //    //public static void Palindrome()
+        //    //{
+
+        //    //        int n, r, sum = 0, temp;
+        //    //        Console.Write("Enter the Number: ");
+        //    //        n = int.Parse(Console.ReadLine());
+        //    //        temp = n;
+        //    //        while (n > 0)
+        //    //        {
+        //    //            r = n % 10;
+        //    //            sum = (sum * 10) + r;
+        //    //            n = n / 10;
+        //    //        }
+        //    //        if (temp == sum)
+        //    //            Console.Write("Number is Palindrome.");
+        //    //        else
+        //    //            Console.Write("Number is not Palindrome");
+
+
+        //    //}
+
+        //    //public static void Solution7()
+        //    //{
+        //    //    string value, reversedValue = "";
+        //    //    Console.Write("Enter a string : ");
+        //    //    value = Console.ReadLine();
+        //    //    if (value != null)
+        //    //    {
+        //    //        for (int i = value.Length - 1; i >= 0; i--)
+        //    //        {
+        //    //            reversedValue += value[i].ToString();
+        //    //        }
+        //    //        if (reversedValue == value)
+        //    //        {
+        //    //            Console.WriteLine("String is a Palindrome String: {0} and Reversed String is: {1}", value, reversedValue);
+        //    //        }
+        //    //        else
+        //    //        {
+        //    //            Console.WriteLine("String is not a Palindrome String: {0} and Reversed String: {1}", value, reversedValue);
+        //    //        }
+        //    //    }
+
+        //    //}
+
+
+
+        //
+
+        public static void TaskDictionnary()
+        {
+            IDictionary<string, string> ditari = new Dictionary<string, string>();
+            ditari.Add("Username", "ABBA");
+            ditari.Add("Password", "Norway@12");
+
+            foreach (KeyValuePair<string, string> kvp in ditari)
+            {
+                if (ditari.ContainsKey("Username"))
+                {
+                    ditari[kvp.Key] = kvp.Value;
+                }
+                Console.WriteLine(kvp.Value);
+                Console.WriteLine(kvp.Key);
+                
+            }
+                //Console.WriteLine("Key: {0}, Value: {1}", kvp.Key, kvp.Value);
+        }
+    }
+}
+
--- HomeWorkProject/HomeWorkProject/Program.cs
+++ HomeWorkProject/HomeWorkProject/Program.cs
@@ -1,12 +1,61 @@
-﻿
+﻿using System.Collections.Generic;
 using HomeWorkProject;
 
-public class SDA
+
+public class Kl_Task3
+
 {
     public static void Main()
     {
+
+        //    //Solution.Solution1();
+        //    //Solution.Solution7();
+        Solution.TaskDictionnary();
+        //}
+
+        //public static void Main()
+        //{
+        //    int i, n;
+        //    int[] a = new int[100];
+
+        //    Console.Write("\n\nRead n number of values in an array and display it in reverse order:\n");
+        //    Console.Write("------------------------------------------------------------------------\n");
+
+        //    Console.Write("Input the number of elements to store in the array :");
+        //    n = Convert.ToInt32(Console.ReadLine());
+
+
+        //    Console.Write("Input {0} number of elements in the array :\n", n);
+        //    for (i = 0; i < n; i++)
+        //    {
+        //        Console.Write("element - {0} : ", i);
+        //        a[i] = Convert.ToInt32(Console.ReadLine());
+        //    }
+
+        //    Console.Write("\nThe values store into the array are : \n");
+        //    for (i = 0; i < n; i++)
+        //    {
+        //        Console.Write("{0}  ", a[i]);
+        //    }
+
+        //    Console.Write("\n\nThe values store into the array in reverse are :\n");
+        //    for (i = n - 1; i >= 0; i--)
+        //    {
+        //        Console.Write("{0} ", a[i]);
+        //    }
+        //    Console.Write("\n\n");
+        //}
+
+
+
         //Ky kodi therret zgjidhjen nga klasa solution task 1
-        Task1.Solution();
-        //Homework1.Solution();
+        //Task1.Solution();
+        //    //Homework1.Solution();
+        //    Console.WriteLine();
+
+
     }
 }
+
+
+
