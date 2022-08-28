e7d6862c0b2b22f65bffd9e93dc09ff1e2f4f66e
Armir <armir.keta@gmail.com>
2022-08-28 14:49:41 +0200

    modifications

--- HomeWorkProject/HomeWorkProject/A_K_Task3.cs
+++ HomeWorkProject/HomeWorkProject/A_K_Task3.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace HomeWorkProject
@@ -35,10 +36,10 @@ namespace HomeWorkProject
             string sSize = Console.ReadLine();
             int arrSize = Convert.ToInt32(sSize);
             int[] arr = new int[arrSize];
-            
+
 
             Console.WriteLine("Input " + arrSize + " elements in the array :\n");
-            
+
 
             for (int i = 0; i < arr.Length; i++)
             {
@@ -47,10 +48,10 @@ namespace HomeWorkProject
             }
 
             Console.WriteLine("\nElements in array are: ");
-            
+
             for (int i = 0; i < arr.Length; i++)
             {
-                Console.WriteLine(arr[i] + "\n");                   
+                Console.WriteLine(arr[i] + "\n");
             }
 
             Console.WriteLine("\nElements in array displayed in reverse order are: ");
@@ -81,15 +82,15 @@ namespace HomeWorkProject
                     Console.WriteLine("String is not a Palindrome String: {0} and Reversed String: {1}", value, reversedValue);
                 }
             }
-            
+
         }
 
         public static void Solution4()
         {
             Dictionary<string, string> userData = new Dictionary<string, string>();
-            userData.Add("Armir","armir2022");
-            userData.Add("Reni","reni2022");
-            userData.Add("Bledi","bledi2022");
+            userData.Add("Armir", "armir2022");
+            userData.Add("Reni", "reni2022");
+            userData.Add("Bledi", "bledi2022");
 
             Console.WriteLine("Input name: ");
             String input = Console.ReadLine();
@@ -135,5 +136,21 @@ namespace HomeWorkProject
 
         }
 
+        public static void Solution6()
+        {
+            Console.WriteLine("Please insert the email address of the user: ");
+            string email = Console.ReadLine();
+
+            bool isEmail = Regex.IsMatch(email, @"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z", RegexOptions.IgnoreCase);
+
+            if (!isEmail)
+            {
+                Console.WriteLine("Email address not valid!");
+            }
+            else
+            {
+                Console.WriteLine("The address entered is a valid email");
+            }
+        }
     }
 }
