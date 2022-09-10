940c2f1adb22dcc0b5f9386df03642969d8904f2
AG <AG@DESKTOP-O9E3P30>
2022-09-10 13:51:10 +0200

    punimi dites

---
+++ HomeWorkProject/HomeWorkProject/D_Sh_ClassToTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkProject
+{
+    public class D_Sh_ClassToTest
+    {
+        public bool FileExists(string fileName)
+        {
+            if (string.IsNullOrEmpty(fileName))
+            {
+                throw new ArgumentNullException("filename");
+            }
+
+
+            var result = File.Exists(fileName);
+
+            return File.Exists(fileName);
+            return result;
+        }
+        public bool StringCompare(string stringName1, string stringName2)
+        {
+            if (stringName1.Equals(stringName2))
+                return true;
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
--- HomeWorkProject/TestProjectHomeWork/DenisaTest.cs
+++ HomeWorkProject/TestProjectHomeWork/DenisaTest.cs
@@ -1,11 +1,73 @@
+using HomeWorkProject;
+
 namespace TestProjectHomeWork
 {
     [TestClass]
     public class DenisaTest
     {
+        public TestContext TestContext { get; set; }
+
+
         [TestMethod]
-        public void TestMethod1()
+        public void FileNameExists()
         {
+            D_Sh_ClassToTest test = new D_Sh_ClassToTest();
+            TestContext.WriteLine("kemi aksesuar klasen me sukses");
+            test.FileExists("");
+
+            bool fromcall;
+
+            fromcall = test.FileExists(@"C:\Windows\regedit.exe");
+
+            TestContext.WriteLine("kemi aksesuar metoden me sukses");
+            Assert.IsTrue(fromcall);
+        }
+
+        public void FileNameNotExists()
+        {
+            D_Sh_ClassToTest test = new D_Sh_ClassToTest();
+            TestContext.WriteLine("kemi aksesuar klasen me sukses");
+            test.FileExists("");
+
+            bool fromcall;
+        
+            fromcall = test.FileExists(@"C:\Windows\regedit.exe");
+
+            TestContext.WriteLine("kemi aksesuar metoden me sukses");
+            Assert.IsFalse(fromcall);
+        }
+
+        [TestMethod]
+        public void TestNotCompiled()
+        {
+            Assert.Inconclusive();
+        }
+        
+        [TestMethod]
+        public void FileNameNullOrEmpty_Try_Catch()
+        {
+            D_Sh_ClassToTest classToTest = new D_Sh_ClassToTest();
+            try
+            {
+                classToTest.FileExists("null");
+            }
+            catch (ArgumentNullException ex)
+            {
+                TestContext.WriteLine("//test was secess is throw exception");
+                return;
+            }
+            Assert.Fail("call to method did not throw an exception");
+        }
+
+        [TestMethod]
+        public void AreEqualsStrings()
+        {
+            D_Sh_ClassToTest test = new D_Sh_ClassToTest();
+            TestContext.WriteLine("Kemi aksesuar klasen me sukses");
+            bool fromcall;
+            fromcall = test.StringCompare("Denisa", "Denisa");
+            TestContext.WriteLine("kemi aksesuar metoden me sukses");
+            Assert.IsTrue(fromcall);
         }
     }
-}
+    }
