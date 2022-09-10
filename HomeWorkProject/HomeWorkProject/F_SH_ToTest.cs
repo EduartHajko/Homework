a7d125563648c7bb2f2f45bc86b5b3830935b724
shytifranko99@outlook.com <shytifranko99@outlook.com>
2022-09-10 12:36:50 +0200

    franko

---
+++ HomeWorkProject/HomeWorkProject/F_SH_ToTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkProject
+{
+    public class F_SH_ToTest
+    {
+        public bool FileExists(string fileName)
+        {
+            if (string.IsNullOrEmpty(fileName))
+            {
+                throw new ArgumentNullException("filename");
+            }
+             
+               var result= File.Exists(fileName);
+            return result;
+        }
+        public bool StringCompare(string stringName1, string stringName2)
+        {
+            if(stringName1.ToLower().Equals(stringName2.ToLower()))
+               
+                return true;
+         else
+            {
+                return false;
+            }
+        }
+    }
+}
--- HomeWorkProject/TestProjectHomeWork/FrankoTests.cs
+++ HomeWorkProject/TestProjectHomeWork/FrankoTests.cs
@@ -1,12 +1,71 @@
+using HomeWorkProject;
+using System;
+
 namespace TestProjectHomeWork
 {
     [TestClass]
     public class FrankoTests
     {
+        public TestContext TestContext { get; set; }
         [TestMethod]
-        public void TestMethod1()
+        public void FileNameExist()
         {
+            F_SH_ToTest test = new F_SH_ToTest();
+            TestContext.WriteLine("Kemi aksesuar klasen me sukses");
+            bool fromcall;
+            fromcall = test.FileExists(@"C:\Windows\regedit.exe");
+            TestContext.WriteLine("kemi aksesuar metoden me sukses");
+            Assert.IsTrue(fromcall);
+        }
+        [TestMethod]
+        public void FileNameDoesNotExist()
+        {
+            F_SH_ToTest test = new F_SH_ToTest();
+            TestContext.WriteLine("Kemi aksesuar klasen me sukses");
+            bool fromcall;
+            fromcall = test.FileExists(@"C:\Windows\regedit.exe");
+            TestContext.WriteLine("kemi aksesuar metoden me sukses");
+            Assert.IsTrue(fromcall);
+        }
+        [TestMethod]
+        public void TestNotCompiled()
+        {
+            Assert.Inconclusive();
+        }
+        //Dua te testoj erroret duke perdorur try catch
+        [TestMethod]
+        public void FileNameNullorEmpty_Try_Catch()
+        {
+            F_SH_ToTest test = new F_SH_ToTest();
+            //Assert.Inconclusive();
+            try
+            {
+                test.FileExists(null);
+            }
+            catch (ArgumentNullException)
+            {
+                TestContext.WriteLine("test was succesful it throw exeption");
+                return;
+            }
+                Assert.Fail("Call to method did not throw an exeption");
 
+            }
+        [TestMethod]
+        public void StringIsEqualOrNot()
+        {
+            string str1 = "franko";
+            string str2 = "Franko";
+            Assert.AreEqual(str1 , str2, true);
+        }
+        [TestMethod]
+        public void AreEqualsStrings()
+        {
+            F_SH_ToTest test = new F_SH_ToTest();
+            TestContext.WriteLine("Kemi aksesuar klasen me sukses");
+            bool fromcall;
+            fromcall = test.StringCompare("franko", "Franko");
+            TestContext.WriteLine("kemi aksesuar metoden me sukses");
+            Assert.IsTrue(fromcall);
         }
     }
-}
+    }
