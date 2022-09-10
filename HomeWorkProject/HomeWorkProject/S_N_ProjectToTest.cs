2a3b74170d47c51173ab05179178b18fd3a79832
serinndroqi <ndroqiserinn@gmail.com>
2022-09-10 12:32:25 +0200

    Ushtirmi 2

--- HomeWorkProject/HomeWorkProject/S_N_ProjectToTest.cs
+++ HomeWorkProject/HomeWorkProject/S_N_ProjectToTest.cs
@@ -16,5 +16,23 @@ namespace HomeWorkProject
             }
             return File.Exists(fileName);
         }
+
+        public bool StringCompare(string stringName1, string stringName2)
+        {
+            bool result = false;
+            if (stringName1.ToLower().Equals(stringName2.ToLower()))
+                result = true;
+
+            return result;
+        }
+
+
+
+
+
+
+
     }
+
+
 }
--- HomeWorkProject/TestProjectHomework/S_Testing.cs
+++ HomeWorkProject/TestProjectHomework/S_Testing.cs
@@ -7,6 +7,7 @@ namespace TestProjectHomework
     public class S_Testing
     {
         public TestContext TestContext { get; set; }
+
         [TestMethod]
         public void FileNameExists()
         {
@@ -21,5 +22,49 @@ namespace TestProjectHomework
             Assert.IsTrue(fromcall);
 
         }
+
+        [TestMethod]
+        public void TestNotCompiled()
+        {
+            Assert.Inconclusive();
+        }
+        [TestMethod]
+        // dua te testoj errors duke perdorur try catch
+        public void FileNameNullOrEmpty_Try_Catch()
+        {
+            S_N_ProjectToTest test = new S_N_ProjectToTest();
+            try
+            {
+
+            }
+            catch (ArgumentNullException)
+            {
+                TestContext.WriteLine("//Test was successful it threw exeption:");
+
+                return;
+            }
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void AreEqualsStrings()
+        {
+            S_N_ProjectToTest test = new S_N_ProjectToTest();
+            TestContext.WriteLine("Kemi aksesuar klasen me sukses");
+            bool fromcall;
+            fromcall = test.StringCompare("edi","Edi");
+            TestContext.WriteLine("Kemi aksesuar metoden me sukses");
+            Assert.IsTrue(fromcall);
+            
+        }
+
+
+
+
+
+
+
+
+
     }
 }
