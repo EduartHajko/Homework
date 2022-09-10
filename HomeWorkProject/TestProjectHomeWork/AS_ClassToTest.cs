e1c85661452e478ef96765500d5b66535fd14ad7
Alex Sinani <sinanialex383@gmail.com>
2022-09-10 12:03:11 +0200

    Test

---
+++ HomeWorkProject/TestProjectHomeWork/AS_ClassToTest.cs
@@ -0,0 +1,69 @@
+﻿using HomeWorkProject;
+using TestProjectHomeWork;
+
+namespace TestProjectHomeWork
+{
+    [TestClass]
+    public class AS_ClassToTest
+    {
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        public void FileNameDoesNotExists()
+        {
+            AlexSinaniTest test = new AlexSinaniTest();
+            TestContext.WriteLine("Kemi aksesuar klasen me sukses:");
+            bool fromcall;
+            fromcall = test.FileExists(@"C:\Windows\kridens.exe");
+            TestContext.WriteLine("Kemi aksesuar metoden me sukses:");
+
+            Assert.IsFalse(fromcall);
+
+        }
+        [TestMethod]
+        public void TestNotCompiled()
+        {
+            Assert.Inconclusive();
+        }
+        [TestMethod]
+        public void FileNameExists()
+        {
+            AlexSinaniTest test = new AlexSinaniTest();
+            TestContext.WriteLine("Kemi aksesuar klasen me sukses:");
+            bool fromcall;
+            fromcall = test.FileExists(@"C:\Windows\regedit.exe");
+            TestContext.WriteLine("Kemi aksesuar metoden me sukses:");
+
+            Assert.IsTrue(fromcall);
+
+        }
+        [TestMethod] // per te pare erroret me try catch
+        public void FileNameNullOrEmpty_Try_Catch()
+        {
+            AlexSinaniTest test = new AlexSinaniTest();
+            try
+            {
+                test.FileExists(null);
+            }
+            catch (ArgumentNullException)
+            {
+                TestContext.WriteLine("//test was success it throws expection");
+                return;
+            }
+            Assert.Fail("call to method did not throw an exception");
+        }
+        [TestMethod]
+        public void StringAreEqualOrNot()
+        {
+            AlexSinaniTest test = new AlexSinaniTest();
+            //Assert.Inconclusive();
+
+            string st1 = "alex";
+            string st2 = "Alex";
+
+            Assert.AreEqual(st1, st2);
+        }
+    }
+}
+
+
